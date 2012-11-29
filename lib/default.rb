3d435cf79d36c89c900d34994054a12edd618dd6
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2012-11-29 11:40:30 +0100

    Build Stucture folder & YAML

--- lib/default.rb
+++ lib/default.rb
@@ -1,3 +1,7 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 include Nanoc3::Helpers::Capturing
+
+def vines
+  @vines ||= items.select {|item| item.identifier =~ /^\/vins/ }.sort {|a, b| a[:order] <=> b[:order] }
+end
