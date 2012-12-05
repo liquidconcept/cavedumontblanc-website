586d9c842c75f104650ce48f5c6e51b9463db95e
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2012-12-05 10:51:45 +0100

    Balls slider animate

--- content/javascripts/default.js
+++ content/javascripts/default.js
@@ -10,6 +10,8 @@
     slideDuration: 800,
     slideInterval: 4500,
 
+    items: [],
+
     _timer: null,
     _activeSlide: false,
 
@@ -71,44 +73,59 @@
 
     // slide function
     _slide: function(nextItem) {
-      this._activeSlide = true;
+      var that = this;
 
+      this._activeSlide = true; // slide is active
+
+      // get current item
       var visibleItem = $(this.items).filter(':visible');
 
+      // get item following current item if not set
       if (nextItem === undefined) {
         nextItem = visibleItem.next();
 
         if (nextItem.length === 0) {
           nextItem = $(this.items).first();
         }
+      // interupt slide if next item is already visible
       } else if (nextItem.is(':visible')) {
-        this._activeSlide = false;
+        this._activeSlide = false; // slide is no more active
         return;
+      // else ensure next item is a jQuery collection
       } else {
         nextItem = $(nextItem);
       }
 
+      // move next item at right of current item
       nextItem.css('left', this.width + 'px');
 
-      var that = this;
+      // find index of balls for current & next item
+      var visibleBallIndex = Math.abs($(this.items).index(visibleItem) - this.items.length + 1);
+      var nextBallIndex = Math.abs($(this.items).index(nextItem) - this.items.length + 1);
+
+      // animate balls
+      _.each([$('#promotions .balls').slice(visibleBallIndex, visibleBallIndex + 1), $('#promotions .balls').slice(nextBallIndex, nextBallIndex + 1)], function(ball) {
+        ball.toggleClass('active', this.slideDuration);
+      }, this);
+
+      // animate items
       _.each([visibleItem, nextItem], function(item) {
         item.show().animate({
           left:  '-=' + this.width + 'px'
         },
         this.slideDuration,
         function() {
-          visibleItem.hide();
-          that._activeSlide = false;
+          visibleItem.hide();         // ensure old current item is hidden
+          that._activeSlide = false;  // slide is no more active
         });
       }, this);
     }
   });
 
-  /* init */
-
+  // init
   $(function() {
+    // slider
     var slider = window.slider = new Slider();
-
     slider.start();
   });
 })(jQuery);
