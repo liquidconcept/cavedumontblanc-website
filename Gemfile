d3bb6fbbd940a731f2855934d59c19ad37b36aee
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2012-12-13 16:48:24 +0100

    update deploy script to use copy strategy

--- Gemfile
+++ Gemfile
@@ -2,22 +2,24 @@
 source "http://rubygems.org"
 
 gem 'rack'
-gem 'nanoc'
 gem 'sinatra'
 gem 'pony'
 
-gem 'sprockets'
-gem 'sprockets-sass'
-gem 'sprockets-helpers'
-gem 'sass'
-gem 'compass'
-gem 'uglifier'
-gem 'libv8', '~> 3.11.8'
-gem 'therubyracer'
-gem 'nanoc-sprockets-filter'
-gem 'nanoc-gzip-filter'
-
 group :production, :staging do
+end
+
+group :nanoc do
+  gem 'nanoc'
+  gem 'sprockets'
+  gem 'sprockets-sass'
+  gem 'sprockets-helpers'
+  gem 'sass'
+  gem 'compass'
+  gem 'uglifier'
+  gem 'libv8', '~> 3.11.8'
+  gem 'therubyracer'
+  gem 'nanoc-sprockets-filter'
+  gem 'nanoc-gzip-filter'
 end
 
 group :development do
--- config/deploy.rb
+++ config/deploy.rb
@@ -13,26 +13,32 @@ default_run_options[:pty] = true
 default_environment['LC_CTYPE'] = 'en_US.UTF-8'
 
 set :user,        'webpublisher'
-set :deploy_via,  :remote_cache
+set :deploy_via,  :copy
 set :deploy_to,   "/var/www/#{domain}/#{application}"
 set :use_sudo,    false
+
+set :bundle_without, [:nanoc, :development, :test, :guard]
 
 role :web, server_name                          # Your HTTP server, Apache/etc
 role :app, server_name                          # This may be the same as your `Web` server
 role :db,  server_name, :primary => true        # This is where Rails migrations will run
 
+set :copy_exclude, ".git/*"
+set :build_script, "bundle install && RACK_ENV=\"production\" bundle exec nanoc compile"
+
 namespace :deploy do
   task :start, :roles => :app do
+    run "mkdir -p #{File.join(current_path,'tmp')}"
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
+    run "mkdir -p #{File.join(current_path,'tmp')}"
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
 
 before 'deploy:symlink' do
-  run "cd #{release_path} && RACK_ENV=\"production\" bundle exec nanoc compile > /dev/null"
 end
 
 after 'deploy:update', 'deploy:restart'
