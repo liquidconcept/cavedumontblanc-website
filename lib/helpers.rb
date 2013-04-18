bad0b0c6288e83bda2081a4414e3cb9bb8247eab
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2013-04-18 15:57:39 +0200

    Add new wine & archive feature for each wines

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,7 +5,7 @@ include Nanoc::Helpers::Capturing
 include Nanoc::Helpers::Sprockets
 
 def wines
-  @wines ||= items.select {|item| item.identifier =~ /^\/wines/ }.sort {|a, b| a[:order] <=> b[:order] }
+  @wines ||= items.select {|item| item.identifier =~ /^\/wines/ }.reject {|w| w[:archive] }.sort {|a, b| a[:order] <=> b[:order] }
 end
 
 def wine_image_path(item, options = {})
